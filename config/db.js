df7bf50b55d7d20a9cbd5c32272432ff5794cbd8
LeeBiri <beeny2000@gmail.com>
2024-12-20 13:34:44 +0900

    project2

--- config/db.js
+++ config/db.js
@@ -1,16 +1,9 @@
 const mongoose = require("mongoose");
 
-const connectDB = async () => {
-	try {
-		await mongoose.connect(process.env.DATABASE_URI, {
-			useNewUrlParser: true,
-			useUnifiedTopology: true,
-		});
-		console.log("MongoDB 연결 성공");
-	} catch (error) {
-		console.error("MongoDB 연결 실패:", error);
-		process.exit(1); // 연결 실패 시 애플리케이션 종료
-	}
-};
-
-module.exports = connectDB;
+mongoose
+	.connect(process.env.MONGODB_URI, {
+		useNewUrlParser: true,
+		useUnifiedTopology: true,
+	})
+	.then(() => console.log("MongoDB 연결 성공!"))
+	.catch((err) => console.error("MongoDB 연결 실패:", err));
